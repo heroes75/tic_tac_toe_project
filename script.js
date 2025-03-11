2f929bffb151164974e34e79e671b9558d893b67
heroes75 <123456789+odin@users.noreply.github.com>
2025-03-11 21:39:40 +0000

    start to add a tie function

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ const Gameboard = (function()  {
         ["", "", ""],
         ["", "", ""],
     ]
-    
+    const resetBoard = () => {
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array.length; j++) {
+                array[i][j] = "";
+                
+            }
+            
+        }
+    }
     const getBoard = () => array
-    return {array, getBoard, isAlreadyMarked}
+    return {array, getBoard, isAlreadyMarked, resetBoard}
 })();
 console.log(Gameboard.getBoard())
 const displayControler = (function() {
@@ -17,28 +25,41 @@ const displayControler = (function() {
         return activeUser = activeUser === arrayOfUser[0] ? arrayOfUser[1] : arrayOfUser[0];
     }
     const playRound = () => {
+        console.log(`${josh.name}: ${josh.getScore()} || ${mosh.name}: ${mosh.getScore()}`);
         console.log(`${activeUser.name}'s turn`);
     }
+    const newRound = () => {
+        Gameboard.resetBoard();
+        activeUser = arrayOfUser[0];
+        playRound();
+    }
     const getActiveUser = () => activeUser;
+    const isTie = () => {
+
+    }
     const isWinner = (marker) => {
         if(Gameboard.array.some(el => el[0]=== el[1] && el[1] === el[2] && el[0] === marker)){
-            console.log("Winner !!!!!!!!!")
+            return true;
         }
         for (let i = 0; i < 3; i++) {
             const j = 0;
             console.log(Gameboard.array[j][i] === Gameboard.array[j+1][i] && Gameboard.array[j+1][i] === Gameboard.array[j+2][i] && Gameboard.array[j][i] === marker)
             if (Gameboard.array[j][i] === Gameboard.array[j+1][i] && Gameboard.array[j+1][i] === Gameboard.array[j+2][i] && Gameboard.array[j][i] === marker) {
-                console.log("Winner !!!!!!!!! vertival") 
+                return true; 
             }
-            
+        }
+        if ((Gameboard.array[1][1] === Gameboard.array[2][2] && Gameboard.array[2][2] === Gameboard.array[0][0] && Gameboard.array[2][2] === marker) ||
+         (Gameboard.array[0][2] === Gameboard.array[1][1] && Gameboard.array[1][1] === Gameboard.array[2][0] && Gameboard.array[1][1] === marker)) {
+            return true;
         }
     }
     playRound();
 
-    return {playRound, changeActiveUser, getActiveUser, isWinner}
+    return {playRound, changeActiveUser, getActiveUser, isWinner, newRound}
 })()
 function createPlayer(name, marker) {
     let score = 0;
+    const getScore = () => score;
     const markTheGameboard = (x, y) => {
         if (Gameboard.isAlreadyMarked(x, y)) {
             console.log("is Already Marked");
@@ -46,12 +67,17 @@ function createPlayer(name, marker) {
             return
         }
         Gameboard.array[x][y] = marker;
-        displayControler.isWinner(marker)
+        if (displayControler.isWinner(marker)) {
+            console.log(`the winner of this round is ${name}`);
+            score++;
+            displayControler.newRound();
+            return
+        }
         displayControler.changeActiveUser();
         displayControler.playRound();
         console.log(Gameboard.array);
     }
-    return {name, marker, markTheGameboard}
+    return {name, marker, markTheGameboard, getScore}
 }
 
 //const josh = createPlayer("josh", "O");
